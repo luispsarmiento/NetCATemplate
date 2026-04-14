84ad19ada4a0b7fff33dcc6d5cbba01403ddff38
Luis Puc <contacto@luispsarmiento.com>
2026-04-13 23:24:25 -0600

    Changed: implement database query in GetTodosQueryHandler using EF Core

--- NetCATemplate.Application/Todos/Get/GetTodosQueryHandler.cs
+++ NetCATemplate.Application/Todos/Get/GetTodosQueryHandler.cs
@@ -1,45 +1,26 @@
-﻿using NetCATemplate.Application.Abstractions.Messaging;
+﻿using Microsoft.EntityFrameworkCore;
+using NetCATemplate.Application.Abstractions.Data;
+using NetCATemplate.Application.Abstractions.Messaging;
 using NetCATemplate.Application.Todos.Get;
 using NetCATemplate.SharedKernel;
 
 namespace NetCATemplate.Application.Todos.Get;
 
-internal sealed class GetTodosQueryHandler()
+internal sealed class GetTodosQueryHandler(IApplicationDbContext dbContext)
     : IQueryHandler<GetTodosQuery, List<TodoResponse>>
 {
     public async Task<Result<List<TodoResponse>>> Handle(GetTodosQuery query, CancellationToken cancellationToken)
     {
-        var sampleTodos = new List<TodoResponse> {
-            new TodoResponse
+        var sampleTodos = await dbContext.TodoItems
+            .Select(x => new TodoResponse
             {
-                Id = 1, 
-                Title = "Walk the dog"
-            },
-            new TodoResponse
-            {
-                Id = 2, 
-                Title = "Do the dishes", 
-                DueBy = DateOnly.FromDateTime(DateTime.Now)
-            },
-            new TodoResponse
-            {
-                Id = 3,
-                Title = "Do the laundry", 
-                DueBy = DateOnly.FromDateTime(DateTime.Now.AddDays(1))
-            },
-            new TodoResponse
-            {
-                Id = 4,
-                Title = "Clean the bathroom"
-            },
-            new TodoResponse
-            {
-                Id = 5,
-                Title = "Clean the car", 
-                DueBy = DateOnly.FromDateTime(DateTime.Now.AddDays(2))
-            }
-        };
-        await Task.CompletedTask;
+                Id = x.Id,
+                Title = x.Description,
+                DueBy = x.DueDate.HasValue ? DateOnly.FromDateTime(x.DueDate.Value) : null,
+                IsComplete = x.IsCompleted
+            })
+            .ToListAsync(cancellationToken);
+
         return sampleTodos;
     }
 }
--- NetCATemplate.Application/Todos/Get/TodoResponse.cs
+++ NetCATemplate.Application/Todos/Get/TodoResponse.cs
@@ -2,7 +2,7 @@
 
 public sealed class TodoResponse
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public string? Title { get; set; } 
     public DateOnly? DueBy { get; set; }
     public bool IsComplete {  get; set; }
